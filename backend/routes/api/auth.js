40964fea0d353d94f7328783ddad1c3391c93576
fepu08 <ferencbesenyei@protonmail.com>
2020-12-15 10:00:58 +0100

    user authentication, login route

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route   GET api/auth
 // @desc    Test route
@@ -15,6 +19,54 @@ router.get('/', auth, async (request, response) => {
     console.error(err.message);
     response.status(500).send('Server Error');
   } 
+});
+
+// @route   POST api/auth
+// @desc    Authenticate user and get token
+// @access  Public
+router.post('/', [
+  check('email', 'Please enter a valid email').isEmail(),
+  check('password', 'Password is required').exists()
+], 
+async (request, response) => {
+  const errors = validationResult(request);
+  if(!errors.isEmpty()){
+    return response.status(400).json({errors: errors.array()});
+  }
+  
+  const {email, password} = request.body;
+  try {
+    // See if user exists
+    let user = await User.findOne({email});
+    if(!user){
+      return response.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+      return response.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+    }
+
+    // Return jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload, 
+      config.get('jwtSecret'),
+      {expiresIn: 3600},
+      (err, token) => {
+        if(err) throw err;
+        response.json({token});
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    response.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
