2181cf8bb2d303b1988cf2fb3cf273f50a1680a7
fepu08 <ferencbesenyei@protonmail.com>
2020-12-28 14:10:02 +0100

    created user profile

---
+++ backend/models/Profile.js
@@ -0,0 +1,47 @@
+const modgoose = require('mongoose');
+
+const ProfileSchema = new modgoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  location: [
+    {
+      address: {
+        type: String
+      },
+      city: {
+        type: String
+      },
+      country: {
+        type: String
+      }
+    }
+  ],
+  githubusername: {
+    type: String
+  },
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
