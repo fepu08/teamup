3b45f87f3ab9dd66752ae03f7c87718d59703746
fepu08 <ferencbesenyei@protonmail.com>
2021-01-04 18:58:12 +0100

    backend - team route - get all teams

--- backend/routes/api/teams.js
+++ backend/routes/api/teams.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Team = require('../../models/Team');
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/teams
-// @desc    Test route
+// @route   GET api/teams/
+// @desc    Get all teams
 // @access  Public
-router.get('/', (request, response) => response.send('Teams route'));
+router.get('/', async (request, response) => {
+    try {
+        const teams = await Team.find();
+        response.json(teams);
+    } catch (err) {
+        console.error(err.message);
+        response.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
